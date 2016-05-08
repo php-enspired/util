4c8a146616adc992057b9f061f55ae63824f4880
Adrian <adrian.testaavila@gmail.com>
2016-05-07 21:15:12 -0400

    updated license

--- src/observable/ObservableException.php
+++ src/observable/ObservableException.php
@@ -1,10 +1,21 @@
 <?php
 /**
  * @package    at.util
- * @version    0.3[20151206]
+ * @version    0.4
  * @author     Adrian <adrian@enspi.red>
  * @copyright  2014 - 2016
- * @license    GNU GPL V2 <http://gnu.org/licenses/gpl-2.0.txt>
+ * @license    GPL-3.0 (no later versions permitted)
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License, version 3.
+ *  You MAY NOT apply the terms of any later version of the GPL.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
  */
 declare( strict_types = 1 );
 namespace at\util\exception;
