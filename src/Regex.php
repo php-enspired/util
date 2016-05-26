a06d2c18fb5ad96272039a4cb10a04493532258a
Adrian <adrian.testaavila@gmail.com>
2016-05-25 23:10:05 -0400

    work on regex

--- src/Regex.php
+++ src/Regex.php
@@ -22,13 +22,122 @@ namespace at\util;
 
 class Regex {
 
-  public static function aggregate( string ...$patterns ) : Regex {}
+  /**
+   * @const int[]  list flags to affect Regex behavior.
+   *
+   * @see <http://php.net/pcre.constants>
+   */
+  const MODES = [
+    'PATTERN_ORDER' => PREG_PATTERN_ORDER,
+    'SET_ORDER' => PREG_SET_ORDER,
+    'OFFSET_CAPTURE' => PREG_OFFSET_CAPTURE,
+    'SPLIT_NO_EMPTY' => PREG_SPLIT_NO_EMPTY,
+    'SPLIT_DELIM_CAPTURE' => PREG_SPLIT_DELIM_CAPTURE,
+    'SPLIT_OFFSET_CAPTURE' => PREG_SPLIT_OFFSET_CAPTURE
+  ];
 
-  public static function quote( string $literal ) : string {}
+  /**
+   * @const string[]  allowed modifiers.
+   *
+   * @see <http://php.net/reference.pcre.pattern.modifiers>
+   * additionally supports "MATCH_ALL" ('g') to perform a global match
+   */
+  const MODIFIERS = [
+    'ANCHORED' => 'A',
+    'CASELESS' => 'i',
+    'DOLLAR_ENDONLY' => 'D',
+    'DOTALL' => 's',
+    'EXTENDED' => 'x',
+    'EXTRA' => 'X',
+    'INFO_JCHANGED' => 'J',
+    'MATCH_ALL' => 'g',
+    'MULTILINE' => 'm',
+    'UNGREEDY' => 'U',
+    'UTF8' => 'u'
+  ];
 
-  public static function valid( string $pattern ) : bool {}
+  /**
+   * factory: creates an aggregated regex from multiple patterns.
+   *
+   * @param string …$patterns  the pattern(s) to use
+   * @return Regex             the aggregated regex
+   */
+  public static function aggregate( string ...$patterns ) : Regex {
+    return new self( '^(?|' . implode( '|', $patterns ) . ')$' );
+  }
 
-  public function __construct( string $pattern, array $flags=[] ) {}
+  /**
+   * escapes characters with special meaning in pcre.
+   *
+   * @param string $literal  the string to escape
+   * @return string          the escaped string
+   */
+  public static function quote( string $literal ) : string {
+    return preg_quote( $literal );
+  }
 
-  public function matches( string $subject ) : bool {}
+  /**
+   * checks whether a string is a valid regular expression.
+   *
+   * @param string $pattern  the pattern to validate (sans delimiters + modifiers)
+   * @param string &$error   if pattern is invalid, will contain the specific error message
+   * @return bool            true if pattern is valid; false otherwise
+   */
+  public static function valid( string $pattern, &$error='' ) : bool {
+    if ( @preg_match( "({$pattern})", '' ) === false ) {
+      $error = str_replace( 'preg_match(): ', '', error_get_last()['message']);
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * @type bool  perform a global pattern match? */
+  protected $_matchAll = false;
+
+  /**
+   * @type string  the compiled regular expression. */
+  protected $_pattern;
+
+  /**
+   * @param string   $pattern    regular expression (sans delimiters + modifiers)
+   * @param string[] $modifiers  pattern modifiers
+   */
+  public function __construct( string $pattern, array $modifiers=[] ) {
+    // php pcre doesn't understand g
+    $g = array_search( self::MODIFIERS['MATCH_ALL'], $modifiers );
+    if ( $g !== false ) {
+      $this->_matchAll = true;
+      unset( $modifiers[$g] );
+    }
+    $this->_pattern = "({$pattern})" . implode( $modifiers );
+
+    if ( ! Regex::valid( $this->_pattern, $error ) ) {
+      throw new \InvalidArgumentException( $error, E_WARNING );
+    }
+  }
+
+  /**
+   * checks if a given string matches this pattern.
+   *
+   * @param string $subject  the subject string
+   * @param int    $flags    PREG_OFFSET_CAPTURE
+   * @param int    $offset   byte offset to start matching against subject string
+   * @return bool            true if string matches pattern; false otherwise
+   */
+  public function is_match( string $subject, int $flags=0, int $offset=0 ) : bool {
+    return $this->match( $subject, $flags, $offset )->is_match();
+  }
+
+  /**
+   * performs a pattern match against a given string.
+   * @see <http://php.net/preg_match>
+   * @see <http://php.net/preg_match_all>
+   *
+   * @param string $subject  the subject string
+   * @param int    $flags    PREG_OFFSET_CAPTURE
+   * @param int    $offset   byte offset to start matching against subject string
+   * @return PCREResult      a result object
+   */
+  public function match( string $subject, int $flags=0, int $offset=0 ) : RegexResult {}
 }
